c499b4a2d82ada048a2e4b1ea9dc69d556fd30b0
nickbattani <battaninic@gmail.com>
2019-05-02 11:46:35 -0500

    Lab 5
    Sensor data analysis

---
+++ Lab5/Debug/l5_main.d
@@ -0,0 +1,59 @@
+# FIXED
+
+l5_main.obj: ../l5_main.c
+l5_main.obj: ../lcd.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdio.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_ti_config.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/linkage.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdarg.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/string.h
+l5_main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h
+l5_main.obj: ../timer.h
+l5_main.obj: ../button.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h
+l5_main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+l5_main.obj: ../uart.h
+l5_main.obj: ../open_interface.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdlib.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/math.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_defs.h
+l5_main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_limits.h
+l5_main.obj: ../movement.h
+l5_main.obj: ../triggers.h
+l5_main.obj: ../lab5_scan_data.h
+
+../l5_main.c: 
+../lcd.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdio.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/linkage.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdarg.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/string.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h: 
+../timer.h: 
+../button.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+../uart.h: 
+../open_interface.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdlib.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/math.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_defs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_limits.h: 
+../movement.h: 
+../triggers.h: 
+../lab5_scan_data.h: 
---
+++ Lab5/Debug/subdir_vars.mk
@@ -0,0 +1,80 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+LIB_SRCS += \
+../lab5_scan_data.lib 
+
+C_SRCS += \
+../button.c \
+../l5_main.c \
+../lcd.c \
+../movement.c \
+../open_interface.c \
+../timer.c \
+../tm4c123gh6pm_startup_ccs.c \
+../triggers.c \
+../uart.c 
+
+C_DEPS += \
+./button.d \
+./l5_main.d \
+./lcd.d \
+./movement.d \
+./open_interface.d \
+./timer.d \
+./tm4c123gh6pm_startup_ccs.d \
+./triggers.d \
+./uart.d 
+
+OBJS += \
+./button.obj \
+./l5_main.obj \
+./lcd.obj \
+./movement.obj \
+./open_interface.obj \
+./timer.obj \
+./tm4c123gh6pm_startup_ccs.obj \
+./triggers.obj \
+./uart.obj 
+
+OBJS__QUOTED += \
+"button.obj" \
+"l5_main.obj" \
+"lcd.obj" \
+"movement.obj" \
+"open_interface.obj" \
+"timer.obj" \
+"tm4c123gh6pm_startup_ccs.obj" \
+"triggers.obj" \
+"uart.obj" 
+
+C_DEPS__QUOTED += \
+"button.d" \
+"l5_main.d" \
+"lcd.d" \
+"movement.d" \
+"open_interface.d" \
+"timer.d" \
+"tm4c123gh6pm_startup_ccs.d" \
+"triggers.d" \
+"uart.d" 
+
+C_SRCS__QUOTED += \
+"../button.c" \
+"../l5_main.c" \
+"../lcd.c" \
+"../movement.c" \
+"../open_interface.c" \
+"../timer.c" \
+"../tm4c123gh6pm_startup_ccs.c" \
+"../triggers.c" \
+"../uart.c" 
+
+
